402ad28509dcb75bd2c772e24e42f11d8fc73c53
mdhighfi <matthew.highfield@gmail.com>
2015-12-29 15:54:56 -0800

    Generate rows

--- justified_pics.rb
+++ justified_pics.rb
@@ -1,21 +1,38 @@
 class Collection
-  MIN = 220 / 1020
-  MAX = 330 / 1020
+  SHORTEST = 100 / 1020
+  # SHORTEST = 220 / 1020
+  TALLEST = 330 / 1020
   ORDER_TOLERANCE = 3
 
-  attr_reader :pictures, :rows
+  attr_reader :unassigned, :rows, :assigned
 
-  def initialize(num_pics = 1)
-    @pictures = []
+  def initialize(num_pics = 50)
+    @unassigned = []
     num_pics.times do
-      @pictures << Picture.new
+      @unassigned << Picture.new
     end
 
+    @assigned = []
     @rows = []
     generate_rows
   end
 
   def generate_rows
+    until @unassigned.empty?
+      current_row = Row.new([@unassigned.first])
+      @assigned = @unassigned.shift
+      until (current_row.dimensions > SHORTEST) || @unassigned.empty?
+        current_row.add_pic(@unassigned)
+      end
+      unless @unassigned.empty?
+        if current_row.dimensions > TALLEST
+          # find the skinniest picture and swap it with the widest from the next few
+          # then delete a picture
+        end
+      end
+    end
+  end
 
+  def generate_row
   end
 end
--- row.rb
+++ row.rb
@@ -8,4 +8,25 @@ class Row
   def add_pic(pic)
     @pictures << pic
   end
+
+  def remove_pic(pic)
+    delete pic if @pictures.include?(pic)
+  end
+
+  def dimensions
+    scale_factors =
+     = @pictures.inject(0) { |total, curr| curr.width + total } + 4*@pictures.length
+    height = @pictures.inject(0) { |total, curr| curr.height + total }
+    height / width
+  end
+
+  private
+
+  def min_height
+    @pictures.min { |a, b| a.height <=> b.height }
+  end
+
+  def min_width
+    @pictures.min { |a, b| a.height <=> b.height }
+  end
 end
