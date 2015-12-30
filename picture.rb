9fa86cea97a09ec76533c063bf48aeb65fef997a
mdhighfi <matthew.highfield@gmail.com>
2015-12-29 18:16:07 -0800

    Create ERB for testing

--- picture.rb
+++ picture.rb
@@ -2,11 +2,31 @@ class Picture
   MIN = 400
   MAX = 2000
 
-  attr_reader :width, :height, :displacement
+  attr_reader :width, :height, :displacement, :src
 
   def initialize
     @width = rand(MAX-MIN) + MIN
     @height = rand(MAX-MIN) + MIN
     @displacement = 0
+
+    random_colors = rand_color
+    @src = Faker::Placeholdit.image(
+      "#{@width}x#{@height}",
+      'jpg',
+      random_colors.first,
+      random_colors.last,
+      'Some Custom Text'
+    )
+  end
+
+  private
+
+  def rand_color
+    color = ['', '']
+    digits = (0..9).to_a.map(&:to_s).concat(('a'..'f').to_a)
+    6.times do
+      color[0] << digits.sample
+    end
+    color
   end
 end
